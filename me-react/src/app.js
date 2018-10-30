035f41f1c47cac9dec8ca7631eca98f7a8c6e840
wkd709 <849804274@qq.com>
2018-10-30 16:24:45 +0800

    xx

--- me-react/src/app.js
+++ me-react/src/app.js
@@ -1,51 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link,NavLink ,Switch,Redirect } from "react-router-dom";
+import './style/common/common.scss';
+
 import routes from './router/index';
 
-import './style/common/common.scss';
 import Hello from './views/Hello';
 import NoMatch from './views/404';
 import Home from './views/home';
-const RouteWithSubRoutes = (route) => {
-    console.log(route);
-
-    return (
-        <Route path={route.path} 
-            render={(props) => (
-                // location.pathname == '/' ?
-                //     <Redirect to='/Home'/>
-                // :
-                // <Route component={route.component}/>
-
-                route.computedMatch.isExact ? 
-                    <Route component={route.component}/>
-                    :
-                    <Route path='*' component={NoMatch}/>
-            )}
-        />
-    )
-};
 
 ReactDOM.render(
     <Router>
         <div>
-            <div className='nav'>
-                <ul>
-                    <li><NavLink exact to='/'>home</NavLink></li>
-                    <li><NavLink to='/Hello'>Hello</NavLink></li>
-                    <li><NavLink to='/Hello/d'>Hello/d</NavLink></li>
-                </ul>
-            </div>
             <Switch>
-                {/* {routes.map((item, i) => 
-                    <RouteWithSubRoutes {...item} key={i}/>
-                )} */}
-
-                {/* <Route exact path='/' component={Home} />
-                <Route path='/Hello' component={Hello} /> */}
-                {/* <Route path='/404' component={NoMatch} /> 
-                <Redirect from='*' to='/404'/> */}
+                <Route exact path='/' component={Home} />
+                <Route path='/Hello' component={Hello} />
+                <Route path='/404' component={NoMatch} /> 
+                <Redirect from='*' to='/404'/>
             </Switch>
         </div>
     </Router>,
--- me-react/src/views/home.js
+++ me-react/src/views/home.js
@@ -1,74 +1,20 @@
 import React from 'react';
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
-}
-
-function Avatar(props) {
-  return (
-    <div>
-      <h1>{props.user.name}-{props.user.date.toLocaleTimeString()}</h1>
-    </div>
-  );
-}
+import { BrowserRouter as Router, Route, Link,NavLink ,Switch,Redirect } from "react-router-dom";
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-        user: {
-          name:'游客',
-          date: new Date(),
-        }
-      };
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
-      />
-    );
+    this.state = {};
   }
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
-        <Avatar user={this.state.user}/>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        <div className='nav'>
+          <ul>
+            <li><NavLink exact to='/'>home</NavLink></li>
+            <li><NavLink to='/Hello'>Hello</NavLink></li>
+            <li><NavLink to='/Hello/d'>Hello/d</NavLink></li>
+          </ul>
         </div>
       </div>
     );
