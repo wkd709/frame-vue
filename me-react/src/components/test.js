70a3ed6033d4215bcab597d3285e72e03cc30696
wkd709 <849804274@qq.com>
2018-12-07 11:30:05 +0800

    setState

--- me-react/src/components/test.js
+++ me-react/src/components/test.js
@@ -6,19 +6,21 @@ class Todos extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            ind:0
+            ind:0,
+            list:[
+                {name:'啥啊',completed: false}
+            ]
         }
         this.add = this.add.bind(this);
+        this.addTest = this.addTest.bind(this);
+        this.toggleTest = this.toggleTest.bind(this);
+        this.delTest = this.delTest.bind(this);
     }
-    add(){
-        this.state.ind++;
-        this.setState({ind:this.state.ind});
-        var obj = 'li'+this.state.ind;
-        this.props.add_todo(obj);
-    }
+   
     render(){
         return(
             <div>
+                {/* redux 方法实现 */}
                 <ul>
                     { this.props.test.lists.map((list,index) =>(
                         <li className={list.completed?"active":''} key={index}>
@@ -28,8 +30,50 @@ class Todos extends Component {
                     )) }
                 </ul>   
                 <div className='btn' onClick={this.add}>添加</div>
+                
+                {/* 使用 setState 方法实现 */}
+                <div className='test-div'>
+                    <ul>
+                        { this.state.list.map((item,index) =>(
+                            <li className={item.completed?"active":''} key={index}>
+                                <span className='text' onClick={()=>this.toggleTest(index)}>{item.name}</span>
+                                <span className='del' onClick={()=>this.delTest(index)} >×</span>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className='btn' onClick={this.addTest}>添加</div>
+                </div>
             </div>
         )
+    }
+    add(){
+        this.state.ind++;
+        this.setState({ind:this.state.ind});
+        var obj = 'li'+this.state.ind;
+        this.props.add_todo(obj);
+    }
+
+
+    addTest() {
+        this.state.list.push({name:'wo',completed:false});
+        this.setState({list:this.state.list});
+    }
+    toggleTest(index) {
+        this.state.list.map((item,ind)=>{
+            if (ind == index) {
+                item.completed = true;
+            } else {
+                item.completed = false;
+            }
+        })
+        this.setState({list:this.state.list});
+    }
+    delTest(index) {
+        this.state.list.splice(index,1);
+        this.setState({list:this.state.list});
+    }
+
+    componentDidMount() {//方法在将组件输出呈现给DOM后运行
     }
 }
 //connect() 接收四个参数，它们分别是 mapStateToProps ， mapDispatchToProps， mergeProps 和 options 。
